47d793b90848572969566ce1c64e6207fa4d3087
akokko <zhminecraft@sina.com>
2022-09-12 16:18:59 +0800

    change some img

--- src/components/Art.vue
+++ src/components/Art.vue
@@ -1,9 +1,9 @@
 <template>
-  <div v-loading.fullscreen.lock="fullscreenLoading">
+  <div>
     <el-row :gutter="12">
       <el-col :span="8" v-for="item in artList" :key="item">
-        <el-card shadow="hover">
-          <el-image :src="item"/>
+        <el-card shadow="hover" v-loading="imgLoading">
+          <el-image :src="item"  @load="imgLoaded"/>
         </el-card>
       </el-col>
     </el-row>
@@ -16,20 +16,33 @@ import {ref} from "vue";
   const artList = ref([
       require("@/assets/art/1.jpg"),
       require("@/assets/art/2.jpg"),
-      require("@/assets/art/3.jpg"),
-      require("@/assets/art/4.png"),
-      require("@/assets/art/5.png"),
-      require("@/assets/art/6.jpg"),
+      require("@/assets/art/3.jpg")
   ])
 
-  const fullscreenLoading = ref(true)
+  const imgLoading = ref(true)
 
-  let loading = setInterval(() => {
-    if (document.readyState === 'complete') {
+  for (let index of artList.value) {
+    let name = 'imgLoad' + index
+    console.log(name)
+  }
+
+  const imgLoaded = (e: Event) => {
+    imgLoading.value = false
+    console.log(e)
+  }
+
+  /*const myRef = ref<Document[]>([]);
+
+  const artDomList = (el: Document) => {
+    myRef.value.push(el);
+  };*/
+
+  /*let loading = setInterval(() => {
+    /!*if (document.readyState === 'complete') {
       fullscreenLoading.value = false
       clearInterval(loading)
-    }
-  }, 500)
+    }*!/
+  }, 500)*/
 </script>
 
 <style scoped lang="less">
